b218a3aa7cf5e4f10fe141c6c7a2a1a3323467b9
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2021-07-10 14:55:29 -0400

    feat(hobbies): add carousels to photography page
    - fix styling for mobile devices
    
    re #51

--- src/app/pages/hobbies/hobbies.component.ts
+++ src/app/pages/hobbies/hobbies.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HobbiesComponent implements OnInit {
 
 
-  slides = [
+  japanSlides = [
     {src: '../../../assets/hobbies/japan/Japan00001.jpeg'},
     {src: '../../../assets/hobbies/japan/Japan00002.jpeg'},
     {src: '../../../assets/hobbies/japan/Japan00003.jpeg'},
@@ -26,6 +26,59 @@ export class HobbiesComponent implements OnInit {
     {src: '../../../assets/hobbies/japan/Japan00014.jpeg'},
     {src: '../../../assets/hobbies/japan/Japan00015.jpeg'},
     {src: '../../../assets/hobbies/japan/Japan00016.jpeg'}
+  ];
+
+  photographySlides1 = [
+    {src: '../../../assets/hobbies/photography/photo_set_01_1.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_2.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_3.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_4.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_5.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_6.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_7.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_8.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_9.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_01_10.jpeg'}
+  ];
+
+  photographySlides2 = [
+    {src: '../../../assets/hobbies/photography/photo_set_02_1.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_2.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_3.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_4.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_5.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_6.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_7.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_8.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_9.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_10.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_02_11.jpeg'}
+  ];
+
+  photographySlides3 = [
+    {src: '../../../assets/hobbies/photography/photo_set_03_1.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_2.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_3.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_4.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_5.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_6.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_7.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_8.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_9.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_03_10.jpeg'}
+  ];
+
+  photographySlides4 = [
+    {src: '../../../assets/hobbies/photography/photo_set_04_1.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_2.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_3.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_4.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_5.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_6.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_7.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_8.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_9.jpeg'},
+    {src: '../../../assets/hobbies/photography/photo_set_04_10.jpeg'}
   ];
 
   constructor() { }
