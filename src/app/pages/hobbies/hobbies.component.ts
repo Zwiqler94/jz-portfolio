acceddeb8d6633deb32591e941722a01e39d018c
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2021-07-14 11:32:20 -0400

    feat(hobbies): switch starbucks cards to display using ngFor
    re #51

--- src/app/pages/hobbies/hobbies.component.ts
+++ src/app/pages/hobbies/hobbies.component.ts
@@ -10,75 +10,87 @@ export class HobbiesComponent implements OnInit {
 
 
   japanSlides = [
-    {src: '../../../assets/hobbies/japan/Japan00001.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00002.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00003.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00004.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00005.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00006.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00007.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00008.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00009.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00010.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00011.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00012.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00013.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00014.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00015.jpeg'},
-    {src: '../../../assets/hobbies/japan/Japan00016.jpeg'}
+    { src: '../../../assets/hobbies/japan/Japan00001.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00002.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00003.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00004.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00005.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00006.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00007.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00008.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00009.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00010.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00011.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00012.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00013.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00014.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00015.jpeg' },
+    { src: '../../../assets/hobbies/japan/Japan00016.jpeg' }
   ];
 
   photographySlides1 = [
-    {src: '../../../assets/hobbies/photography/photo_set_01_1.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_2.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_3.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_4.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_5.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_6.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_7.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_8.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_9.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_01_10.jpeg'}
+    { src: '../../../assets/hobbies/photography/photo_set_01_1.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_2.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_3.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_4.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_5.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_6.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_7.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_8.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_9.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_01_10.jpeg' }
   ];
 
   photographySlides2 = [
-    {src: '../../../assets/hobbies/photography/photo_set_02_1.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_2.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_3.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_4.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_5.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_6.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_7.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_8.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_9.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_10.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_02_11.jpeg'}
+    { src: '../../../assets/hobbies/photography/photo_set_02_1.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_2.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_3.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_4.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_5.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_6.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_7.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_8.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_9.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_10.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_02_11.jpeg' }
   ];
 
   photographySlides3 = [
-    {src: '../../../assets/hobbies/photography/photo_set_03_1.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_2.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_3.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_4.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_5.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_6.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_7.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_8.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_9.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_03_10.jpeg'}
+    { src: '../../../assets/hobbies/photography/photo_set_03_1.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_2.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_3.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_4.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_5.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_6.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_7.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_8.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_9.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_03_10.jpeg' }
   ];
 
   photographySlides4 = [
-    {src: '../../../assets/hobbies/photography/photo_set_04_1.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_2.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_3.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_4.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_5.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_6.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_7.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_8.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_9.jpeg'},
-    {src: '../../../assets/hobbies/photography/photo_set_04_10.jpeg'}
+    { src: '../../../assets/hobbies/photography/photo_set_04_1.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_2.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_3.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_4.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_5.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_6.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_7.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_8.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_9.jpeg' },
+    { src: '../../../assets/hobbies/photography/photo_set_04_10.jpeg' }
+  ];
+
+  starbucksDrinks = [
+    { drinkName: 'Ice Caffe Mocha', src: 'https://shorturl.at/OQU36' },
+    { drinkName: 'Caffe Mocha', src: 'https://shorturl.at/tBEU0' },
+    { drinkName: 'Iced Caramel Machiatto', src: 'https://shorturl.at/hHOUZ' }
+    // {drinkName: , src:},
+    // {drinkName: , src:},
+    // {drinkName: , src:},
+    // {drinkName: , src:},
+    // {drinkName: , src:},
+    // {drinkName: , src:},
   ];
 
   constructor() { }
