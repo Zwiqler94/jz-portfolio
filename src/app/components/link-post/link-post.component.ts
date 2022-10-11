a9dd1d928520a6da0f8d59aa608d05d3be7123f6
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2022-10-11 01:48:25 -0400

    fix: destroy lint errors

--- src/app/components/link-post/link-post.component.ts
+++ src/app/components/link-post/link-post.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LinkPreviewService } from 'src/app/services/link-preview.service';
 
-
 @Component({
   selector: 'app-link-post',
   templateUrl: './link-post.component.html',
-  styleUrls: ['./link-post.component.scss']
+  styleUrls: ['./link-post.component.scss'],
 })
 export class LinkPostComponent implements OnInit {
-
   @Input() postTitle: string;
   @Input() postContent: string;
   linkPreviewData: any;
 
-  constructor(private linkPreviewService: LinkPreviewService) {
+  constructor(private linkPreviewService: LinkPreviewService) {}
 
+  get getLinkPreviewData() {
+    return this.linkPreviewData;
+  }
+
+  get hasLinkPreviewData() {
+    return this.getLinkPreviewData !== undefined;
   }
 
   ngOnInit(): void {
@@ -22,22 +26,17 @@ export class LinkPostComponent implements OnInit {
   }
 
   getLinkPreview() {
-    const linkArray = this.postContent.match(/(http|https):\/\/www.[a-z]*.[a-z]*\/[a-zA-z0-9?=]*/);
+    const linkArray = this.postContent.match(
+      /(http|https):\/\/www.[a-z]*.[a-z]*\/[a-zA-z0-9?=]*/
+    );
     if (linkArray !== null) {
-      this.linkPreviewService.getLinkPreview(String(linkArray[0])).subscribe((data: any)=>{
-        {
-          this.linkPreviewData = data;
-        }
-      });
+      this.linkPreviewService
+        .getLinkPreview(String(linkArray[0]))
+        .subscribe((data: any) => {
+          {
+            this.linkPreviewData = data;
+          }
+        });
     }
   }
-
-  get getLinkPreviewData(){
-    return this.linkPreviewData;
-  }
-
-  get hasLinkPreviewData(){
-    return this.getLinkPreviewData !== undefined;
-  }
-
 }
