e09e5781c50d39a5afaf78dd6aedf43ac89c400d
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2023-03-18 00:14:38 -0400

    feature(security): add secret manager

--- src/app/services/link-preview/link-preview.service.ts
+++ src/app/services/link-preview/link-preview.service.ts
@@ -1,20 +1,41 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { lastValueFrom } from 'rxjs';
-// import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import {
+  HttpClient,
+  HttpParams,
+} from '@angular/common/http';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LinkPreviewService {
   private baseUrl = 'https://api.linkpreview.net/';
-  private _apiKey!: string;
-  private params: HttpParams = new HttpParams();
+  private secretName =
+    'projects/518070660509/secrets/LINK_PREVIEW_DEV/versions/latest';
+  private _apiKey = '';
+  private params: HttpParams = new HttpParams().set('key', this.apiKey);
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.getAPIKey().then().catch();
+  }
+
+   get apiKey() {
+    return this._apiKey;
+  }
+   set apiKey(value) {
+    this._apiKey = value;
+  }
+
+  async getAPIKey() {
+    const [apiKeyVersion] =
+      await new SecretManagerServiceClient().accessSecretVersion({
+        name: this.secretName,
+      });
+    this.apiKey = apiKeyVersion.payload!.data!.toString();
+    console.log(this.apiKey);
+  }
 
   get apiKey() {
     return this._apiKey;
