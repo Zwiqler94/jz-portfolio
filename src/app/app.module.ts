9c1774d4eb5891d8cb86c8d7be96534220fb7b28
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2023-03-19 16:13:52 -0400

    fix(ngsw): enable service worker in all envs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -104,7 +104,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
