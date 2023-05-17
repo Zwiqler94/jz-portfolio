9553f7c5e6c56cca488fe04e2b26b10780814d83
Zwiqler94 <Zwiqler94@users.noreply.github.com>
2023-05-16 21:15:45 -0400

    feat(cloud functions): upgrade to FB CFv2
    migration to firebase cloud functions v2
    
    BREAKING CHANGE: refer to
    https://firebase.google.com/docs/functions/2nd-gen-upgrade?hl=en&authuser=0

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,42 +1,41 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
 import * as fbAdmin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import {onRequest} from 'firebase-functions/v2/https';
+import { error, debug } from 'firebase-functions/logger';
 import * as express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import * as cors from 'cors';
+// import { applicationDefault } from 'firebase-admin/app';
+import * as creds from '../credentials.json';
+import { defineSecret } from 'firebase-functions/params';
 
-const a = fbAdmin.initializeApp();
+const fbAdminApp = fbAdmin.initializeApp({
+  credential: fbAdmin.credential.cert({
+    clientEmail: creds.client_email,
+    privateKey: creds.private_key,
+    projectId: creds.project_id,
+  }),
+});
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const secretNameDev = defineSecret('LINK_PREVIEW_DEV')
+const secretNameProd = defineSecret('LINK_PREVIEW_PROD');
+  
 
-const secretNameDev =
-  'projects/518070660509/secrets/LINK_PREVIEW_DEV/versions/latest';
-const secretNameProd =
-  'projects/518070660509/secrets/LINK_PREVIEW_PROD/versions/latest';
-
-const app = express();
+const secretsApp = express();
 const secretRouter = express.Router();
 
-app.use(cors());
+secretsApp.use(cors());
 
 const getSecret = async (req: Request, res: Response) => {
   try {
-    const [apiKey] = await new SecretManagerServiceClient().accessSecretVersion(
-      {
-        name: req.params.prod ? secretNameProd : secretNameDev,
-      }
-    );
-    res.status(200).json({ k: apiKey.payload?.data?.toString() });
-  } catch (error) {
-    console.error(error);
+    const apiKey = req.params.prod ? secretNameProd : secretNameDev;
+    debug({ k: apiKey });
+    res.status(200).json({ k: apiKey });
+  } catch (err) {
+    error(err);
+    res.status(400);
   }
 };
 
@@ -47,26 +46,31 @@ const appCheckGaurd = async (
 ) => {
   const appCheckToken = req.header('X-Firebase-AppCheck');
   const appCheckDebugToken = req.header('X-Firebase-AppCheck-Debug');
-
-  if (appCheckDebugToken) {
-    return next();
-  } else if (!appCheckToken) {
+  const tokenToCheck = appCheckToken ? appCheckToken : appCheckDebugToken;
+  debug({ tokenToCheck, creds, fbAdminApp });
+  if (!tokenToCheck) {
     res.status(401);
-    return next('Unauthorized');
-  } else {
-    try {
-      await a.appCheck().verifyToken(appCheckToken);
-      return next();
-    } catch (err) {
-      console.log(err);
-      res.status(401);
-      return next('Unauthorized');
-    }
+    return next('unauthorized A');
+  }
+
+  try {
+    
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const result = await fbAdminApp.appCheck().verifyToken(tokenToCheck);
+    debug({ a:result.token });
+    return next();
+  } catch (err) {
+    error(err);
+    res.status(401);
+    return next('unauthorized B');
   }
 };
 
 secretRouter.get('/secrets', getSecret);
 
-app.use('/api/v1', appCheckGaurd, secretRouter);
+secretsApp.use('/api/v2', appCheckGaurd, secretRouter);
 
-export const secretService = functions.https.onRequest(app);
+const link
+
+export const secretService2ndGen = onRequest({ cors: true, secrets: [secretNameDev, secretNameProd] }, secretsApp);
+
