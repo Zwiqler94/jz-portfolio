d9d5730d6653122507ad6c552e127ec38b9bb35a
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-06-29 02:09:49 -0400

    build(deps): update deps

---
+++ src/app/components/post-base/post-base.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { LinkPreview } from 'src/app/components/models/link-post';
+import { LinkPreviewService } from 'src/app/services/link-preview/link-preview.service';
+
+@Component({
+  selector: 'app-post-base',
+  templateUrl: './post-base.component.html',
+  styleUrls: ['./post-base.component.scss'],
+})
+export class PostBaseComponent{
+  @Input() postTitle: string;
+  @Input() postContent: string;
+}
