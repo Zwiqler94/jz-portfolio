ef86637f03d2a130621e162a27c5903fd9d0c27c
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-07-02 14:20:37 -0400

    feat(posts): use ngsw to cache linkpreview requests~!!

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -77,5 +77,38 @@ const getPuppyPosts = async (req: Request, res: Response) => {
   }
 };
 
+const getArticlePosts = async (req: Request, res: Response) => {
+  try {
+    const mainPosts = await query(
+      'select post_list.id, post_list.type from post_list inner join articles_feed on articles_feed.post_id=post_list.id'
+    );
+    const fullPostArray: any[] = [];
+    for (const row of mainPosts.rows) {
+      let result;
+      if (row.type === 'text') {
+        result = await query(
+          'select post_list.id, post_list.type, text_post.title, post_list.content ,post_list.created_at, post_list.updated_at from post_list inner join text_post on post_list.id=text_post.list_id where post_list.id=$1 ',
+          [row.id]
+        );
+      } else {
+        result = await query(
+          'select post_list.id, post_list.type, link_post.uri, post_list.content ,post_list.created_at, post_list.updated_at from post_list inner join link_post on post_list.id=link_post.list_id where post_list.id=$1 ',
+          [row.id]
+        );
+      }
+      fullPostArray.push(result.rows[0]);
+    }
+    // mainPosts.rows.forEach((row) => {
+    //   const result = query('select * from post_list where id=$1', [row.post_id]);
+    //   fullPostArray.push(result);
+    // })
+    res.status(200).json(fullPostArray);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+  }
+};
+
 postRouter.get('/main', getMainPosts);
 postRouter.get('/puppy', getPuppyPosts);
+postRouter.get('/articles', getArticlePosts);
