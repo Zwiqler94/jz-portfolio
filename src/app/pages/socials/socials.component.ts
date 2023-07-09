5e35144e9cc13996083dce5df34378ee8baaf669
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-07-09 01:53:33 -0400

    feat(socials): add back socials with IG Posts

--- src/app/pages/socials/socials.component.ts
+++ src/app/pages/socials/socials.component.ts
@@ -24,9 +24,9 @@ export class SocialsComponent implements OnInit, AfterViewInit {
     @Inject(DOCUMENT) private domDocument: any,
     private router: Router
   ) {
-    if (this.router.navigated === true) {
-      location.reload();
-    }
+    // if (this.router.navigated === true) {
+    //   location.reload();
+    // }
   }
 
   ngOnInit(): void {}
@@ -37,39 +37,18 @@ export class SocialsComponent implements OnInit, AfterViewInit {
     } catch (error) {
       console.log(error);
     }
-    this.instagramScript.loaded = false;
-    this.twitterScript.loaded = false;
+    // this.instagramScript.loaded = false;
+    // this.twitterScript.loaded = false;
   }
 
   loadScripts(): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.twitterScript = this.renderer2.createElement('script');
-      this.twitterScript.id = 'twitter-wjs';
-      this.twitterScript.type = 'text/javascript';
-      this.twitterScript.src = 'https://platform.twitter.com/widgets.js';
-      this.twitterScript.text = '';
-      this.twitterScript.onload = () => {
-        this.twitterScript.loaded = true;
-        resolve({ success: 'true' });
-      };
-      this.twitterScript.onerror = () => {
-        resolve({ success: 'false' });
-      };
-
-      this.instagramScript = this.renderer2.createElement('script');
-      this.renderer2.setAttribute(this.instagramScript, 'async', '');
-      this.instagramScript.type = 'text/javascript';
-      this.instagramScript.src = 'https://www.instagram.com/embed.js';
-      this.instagramScript.text = '';
-      this.instagramScript.onload = () => {
-        this.instagramScript.loaded = true;
-        resolve({ success: 'true' });
-      };
-      this.instagramScript.onerror = () => {
-        resolve({ success: 'false' });
-      };
-      this.renderer2.appendChild(this.domDocument.body, this.instagramScript);
-      this.renderer2.appendChild(this.domDocument.body, this.twitterScript);
+      const scriptEl = document.createElement('script');
+      scriptEl.src = 'https://www.instagram.com/embed.js';
+      scriptEl.async = true;
+      scriptEl.type = 'text/javascript';
+      const targeto = document.querySelectorAll<HTMLDivElement>('.insta-posts');
+      targeto.forEach((node: HTMLDivElement) => node.append(scriptEl));
     });
   }
 }
