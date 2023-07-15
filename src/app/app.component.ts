1027673b0e9b896d531a79088ecf78e4fae12232
Jake Z <10226084+Zwiqler94@users.noreply.github.com>
2023-07-15 18:04:15 -0400

    chore: updates check (#283)
    * revert: undo the undos
    
    * revert: undo the undo's undos
    
    * chore: test updates deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarDismiss,
+} from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
-import { ServiceWorkerService } from 'src/app/services/service-worker/service-worker.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jlz-portfolio';
 
   constructor(
-    private serviceWorker: ServiceWorkerService,
+    private swUpdate: SwUpdate,
     private auth: AuthService,
+    private snack: MatSnackBar
   ) {}
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      console.log('Service Worker Enabled')
+      this.swUpdate.versionUpdates
+        .pipe(
+          filter(
+            (evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'
+          )
+        )
+        .subscribe((x) => {
+          if (x) {
+            const d = this.snack.open(
+              'New App Version Detected, Update?',
+              'Yup!'
+            );
+            d.afterDismissed().subscribe((f: MatSnackBarDismiss) => {
+              console.log(f.dismissedByAction);
+              while (!f.dismissedByAction) {
+                this.snack.open('Hurry up and update!', 'UPDATE!');
+              }
+              document.location.reload();
+            });
+          }
+        });
+    }
+  }
 
   logout() {
     this.auth.logout();
