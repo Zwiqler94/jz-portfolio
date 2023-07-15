466bd03a3b62210e08560c2ecdee452c2f63c960
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-07-15 17:52:00 -0400

    ci(github actions): allow deploy on push to dev and main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarDismiss,
+} from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { ServiceWorkerService } from 'src/app/services/service-worker/service-worker.service';
 
@@ -7,13 +15,41 @@ import { ServiceWorkerService } from 'src/app/services/service-worker/service-wo
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jlz-portfolio';
 
   constructor(
-    private serviceWorker: ServiceWorkerService,
+    private swUpdate: SwUpdate,
     private auth: AuthService,
+    private snack: MatSnackBar
   ) {}
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      console.log('Service Worker Enabled')
+      this.swUpdate.versionUpdates
+        .pipe(
+          filter(
+            (evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'
+          )
+        )
+        .subscribe((x) => {
+          if (x) {
+            const d = this.snack.open(
+              'New App Version Detected, Update?',
+              'Yup!'
+            );
+            d.afterDismissed().subscribe((f: MatSnackBarDismiss) => {
+              console.log(f.dismissedByAction);
+              while (!f.dismissedByAction) {
+                this.snack.open('Hurry up and update!', 'UPDATE!');
+              }
+              document.location.reload();
+            });
+          }
+        });
+    }
+  }
 
   logout() {
     this.auth.logout();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -101,7 +101,7 @@ self.FIREBASE_APPCHECK_DEBUG_TOKEN = environment.appCheckDebug;
       return auth;
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
