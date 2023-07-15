83bcab03d71086b3c1493144ae2366749611a4f8
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-07-15 17:57:19 -0400

    chore: revert: undo the undo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  MatSnackBar,
-  MatSnackBarAction,
-  MatSnackBarActions,
-  MatSnackBarDismiss,
-} from '@angular/material/snack-bar';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
-import { filter } from 'rxjs';
+import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 
 @Component({
@@ -14,41 +6,13 @@ import { AuthService } from 'src/app/services/auth-service/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'jlz-portfolio';
 
   constructor(
-    private swUpdate: SwUpdate,
+    private serviceWorker: ServiceWorkerService,
     private auth: AuthService,
-    private snack: MatSnackBar
   ) {}
-
-  ngOnInit(): void {
-    if (this.swUpdate.isEnabled) {
-      console.log('Service Worker Enabled')
-      this.swUpdate.versionUpdates
-        .pipe(
-          filter(
-            (evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'
-          )
-        )
-        .subscribe((x) => {
-          if (x) {
-            const d = this.snack.open(
-              'New App Version Detected, Update?',
-              'Yup!'
-            );
-            d.afterDismissed().subscribe((f: MatSnackBarDismiss) => {
-              console.log(f.dismissedByAction);
-              while (!f.dismissedByAction) {
-                this.snack.open('Hurry up and update!', 'UPDATE!');
-              }
-              document.location.reload();
-            });
-          }
-        });
-    }
-  }
 
   logout() {
     this.auth.logout();
