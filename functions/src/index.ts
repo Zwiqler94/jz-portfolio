e35fa482252eafa2dbe0a93f209b55bf338a18b5
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-07-19 23:12:56 -0400

    chore(functions): update functions to use min access service account
    - remove unneccessary rewrites from firebase.json
     - add -rf to predeploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import cors = require('cors');
 import express = require('express');
 import { onRequest } from 'firebase-functions/v2/https';
-// import { appCheckGaurd,  } from "./middleware";
 import {
   clientCert,
   clientKey,
@@ -13,6 +12,9 @@ import {
 import { limiter } from './middleware';
 import { postRouter } from './db';
 
+
+// This is from Branch JZ-269-135
+
 const app = express();
 app.use(cors());
 const jzPortfolioBackendExpressApp = express.Router();
@@ -23,6 +25,7 @@ jzPortfolioBackendExpressApp.use('/posts', postRouter);
 
 export const jzPortfolioApp = onRequest(
   {
+    serviceAccount: 'jzportfolioapp@jlz-portfolio.iam.gserviceaccount.com',
     cors: true,
     secrets: [secretNameDev, secretNameProd, clientCert, clientKey, serverCA],
   },
