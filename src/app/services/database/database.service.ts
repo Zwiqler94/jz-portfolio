3f90adfd027a57ef0d5ff764740250395b9af69f
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2023-11-18 19:56:31 -0500

    fix(appcheck): check for undefined appcheck

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -21,7 +21,7 @@ export class DatabaseService {
   blockchainPosts: Observable<Post[]>;
 
   postUrl = environment.serviceOptions.postService;
-  _appCheck: any;
+  _appCheck: AppCheck|undefined;
   tokenResult: string;
 
   headers: HttpHeaders = new HttpHeaders();
@@ -52,7 +52,7 @@ export class DatabaseService {
 
   async getAppCheckToken(): Promise<string | AppCheckTokenResult | undefined> {
     try {
-      if (!environment.local) {
+      if (!environment.local && this.appCheck) {
         console.info(this.appCheck);
         this.tokenResult = (await getToken(this.appCheck)).token;
         console.info(this.tokenResult);
