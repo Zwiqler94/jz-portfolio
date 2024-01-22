09265e5d8da3711ddeb3fe1d1495162f7ceb746f
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2024-01-21 23:22:38 -0500

    fix(skills): adjust breakpoints for small screens

--- src/app/pages/about-me/skills/skills.component.ts
+++ src/app/pages/about-me/skills/skills.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { Interface } from 'readline';
 import { Tabs } from 'src/app/interfaces/tabs.model';
+
+interface SkillModel {
+  skill: string;
+  list: string;
+}
 
 @Component({
   selector: 'app-skills',
@@ -9,18 +15,39 @@ import { Tabs } from 'src/app/interfaces/tabs.model';
 export class SkillsComponent extends Tabs {
   @Input() public tabTitle: string;
 
-  private _skillList = [
-    'C',
-    'C++',
-    'Java',
-    'JavaScript',
-    'Node.JS',
-    'Python',
-    'Typescript',
-    'Go',
-    'Ruby',
-    'Haskell',
-    'Prolog',
+  private _skillList: SkillModel[] = [
+    { list: 'Programming', skill: 'C' },
+    { list: 'Programming', skill: 'C++' },
+    { list: 'Programming', skill: 'Java' },
+    { list: 'Programming', skill: 'JavaScript' },
+    { list: 'Programming', skill: 'Node.JS' },
+    { list: 'Programming', skill: 'Python' },
+    { list: 'Programming', skill: 'Typescript' },
+    { list: 'Programming', skill: 'Go' },
+    { list: 'Programming', skill: 'Ruby' },
+    { list: 'Programming', skill: 'Haskell' },
+    { list: 'Programming', skill: 'Prolog' },
+    { list: 'Methodologies', skill: 'Agile Software Development' },
+    { list: 'Methodologies', skill: 'Test-Driven Development' },
+    { list: 'Tools', skill: 'Angular' },
+    { list: 'Tools', skill: 'Chai' },
+    { list: 'Tools', skill: 'Docker' },
+    { list: 'Tools', skill: 'Express' },
+    { list: 'Tools', skill: 'Git' },
+    { list: 'Tools', skill: 'Github' },
+    { list: 'Tools', skill: 'Hyperledger Fabric' },
+    { list: 'Tools', skill: 'IBM Cloud' },
+    { list: 'Tools', skill: 'IBM Carbon Design System' },
+    { list: 'Tools', skill: 'Jenkins' },
+    { list: 'Tools', skill: 'Jira' },
+    { list: 'Tools', skill: 'Kubernetes' },
+    { list: 'Tools', skill: 'Mocha' },
+    { list: 'Tools', skill: 'MongoDB' },
+    { list: 'Tools', skill: 'Supertest' },
+    { list: 'Tools', skill: 'Swagger/OpenAPI 3' },
+    { list: 'Tools', skill: 'Cloud Foundry' },
+    { list: 'Tools', skill: 'Cloudant' },
+    { list: 'Tools', skill: 'Hyperledger Indy (IBM Verify Credentials)' },
   ];
   public get skillList() {
     return this._skillList;
