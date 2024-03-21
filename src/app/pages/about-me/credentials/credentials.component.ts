831b3b9ede1c8f0bb2a5ea7362e88ff352bb24a4
Jake Z <10226084+Zwiqler94@users.noreply.github.com>
2024-03-20 22:07:15 -0400

    Revert "feat (creds): add new badges to credentials page"

--- src/app/pages/about-me/credentials/credentials.component.ts
+++ src/app/pages/about-me/credentials/credentials.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MicrosoftLearnUserProfile } from 'src/app/interfaces/microsoft/microsoft';
 import { Tabs } from 'src/app/interfaces/tabs.model';
-import  credInfo from 'src/assets/credentials/msft_credentials.json';
 
 @Component({
   selector: 'app-credentials',
@@ -9,20 +7,14 @@ import  credInfo from 'src/assets/credentials/msft_credentials.json';
   styleUrls: ['./credentials.component.scss'],
 })
 export class CredentialsComponent extends Tabs implements OnInit {
-  MICROSOFT_LEARN_URL = 'https://learn.microsoft.com/en-us';
-  profile: MicrosoftLearnUserProfile;
-
   ngOnInit(): void {
+    console.log('xxx');
     const scriptEl = document.createElement('script');
     scriptEl.src = 'https://cdn.credly.com/assets/utilities/embed.js';
     scriptEl.async = true;
     scriptEl.type = 'text/javascript';
     const targeto = document.querySelector('#credential-div') as HTMLDivElement;
     targeto.append(scriptEl);
-
-
-    this.profile = new MicrosoftLearnUserProfile(credInfo);
-    console.log(this.profile)
   }
   @Input() tabTitle: string;
 }
