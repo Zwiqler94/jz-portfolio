a9d7629d2dd79a9d75307cebe2e4da42f6b2450e
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2024-03-22 22:55:51 -0400

    fix: add back credential updates

--- src/app/pages/about-me/credentials/credentials.component.ts
+++ src/app/pages/about-me/credentials/credentials.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MicrosoftLearnUserProfile } from 'src/app/interfaces/microsoft/microsoft';
 import { Tabs } from 'src/app/interfaces/tabs.model';
+import  credInfo from 'src/assets/credentials/msft_credentials.json';
 
 @Component({
   selector: 'app-credentials',
@@ -7,14 +9,22 @@ import { Tabs } from 'src/app/interfaces/tabs.model';
   styleUrls: ['./credentials.component.scss'],
 })
 export class CredentialsComponent extends Tabs implements OnInit {
+  MICROSOFT_LEARN_URL = 'https://learn.microsoft.com/en-us';
+  profile: MicrosoftLearnUserProfile;
+
   ngOnInit(): void {
-    console.log('xxx');
     const scriptEl = document.createElement('script');
     scriptEl.src = 'https://cdn.credly.com/assets/utilities/embed.js';
     scriptEl.async = true;
     scriptEl.type = 'text/javascript';
     const targeto = document.querySelector('#credential-div') as HTMLDivElement;
     targeto.append(scriptEl);
+
+
+    this.profile = new MicrosoftLearnUserProfile(credInfo);
+    console.log(this.profile)
   }
   @Input() tabTitle: string;
 }
+
+
