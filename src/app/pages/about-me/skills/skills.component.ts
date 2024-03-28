a8052c0238310415d48e2872d0ad411b92767a9c
Jake Z <10226084+Zwiqler94@users.noreply.github.com>
2024-03-27 23:37:06 -0400

    fix(credentials): update size of badge icons (#444)

--- src/app/pages/about-me/skills/skills.component.ts
+++ src/app/pages/about-me/skills/skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Interface } from 'readline';
 import { Tabs } from 'src/app/interfaces/tabs.model';
+import { environment } from 'src/environments/environment';
 
 interface SkillModel {
   skill: string;
@@ -14,8 +15,26 @@ interface SkillModel {
 })
 export class SkillsComponent extends Tabs {
   @Input() public tabTitle: string;
-
-  private _skillList: SkillModel[] = [
+  private _skillList: SkillModel[];
+  private _skillListDefault: SkillModel[] = [
+    { list: 'Cloud', skill: 'AWS' },
+    { list: 'Cloud', skill: 'Azure' },
+    { list: 'Cloud', skill: 'Google Cloud Platform' },
+    { list: 'Cloud', skill: 'IBM Cloud' },
+    { list: 'Databases', skill: 'IBM Cloudant' },
+    { list: 'Databases', skill: 'MongoDB' },
+    { list: 'Databases', skill: 'PostgreSQL' },
+    { list: 'Designs', skill: 'Angular Material' },
+    { list: 'Designs', skill: 'Bootstrap' },
+    { list: 'Designs', skill: 'IBM Carbon Design System' },
+    { list: 'Frameworks', skill: 'Angular' },
+    { list: 'Frameworks', skill: 'Chai' },
+    { list: 'Frameworks', skill: 'Express' },
+    { list: 'Frameworks', skill: 'Hyperledger Fabric' },
+    { list: 'Frameworks', skill: 'Hyperledger Indy' },
+    { list: 'Frameworks', skill: 'Mocha' },
+    { list: 'Methodologies', skill: 'Agile Software Development' },
+    { list: 'Methodologies', skill: 'Test-Driven Development' },
     { list: 'Programming', skill: 'C' },
     { list: 'Programming', skill: 'C++' },
     { list: 'Programming', skill: 'Go' },
@@ -27,35 +46,25 @@ export class SkillsComponent extends Tabs {
     { list: 'Programming', skill: 'Python' },
     { list: 'Programming', skill: 'Ruby' },
     { list: 'Programming', skill: 'Typescript' },
-    { list: 'Methodologies', skill: 'Agile Software Development' },
-    { list: 'Methodologies', skill: 'Test-Driven Development' },
-    { list: 'Tools', skill: 'Angular' },
-    { list: 'Tools', skill: 'AWS' },
-    { list: 'Tools', skill: 'Chai' },
     { list: 'Tools', skill: 'Cloud Foundry' },
-    { list: 'Tools', skill: 'Cloudant' },
     { list: 'Tools', skill: 'Docker' },
-    { list: 'Tools', skill: 'Express' },
     { list: 'Tools', skill: 'Git' },
     { list: 'Tools', skill: 'Github' },
-    { list: 'Tools', skill: 'Google Cloud Platform' },
-    { list: 'Tools', skill: 'Hyperledger Fabric' },
-    { list: 'Tools', skill: 'Hyperledger Indy' },
-    { list: 'Tools', skill: 'IBM Carbon Design System' },
-    { list: 'Tools', skill: 'IBM Cloud' },
     { list: 'Tools', skill: 'Jenkins' },
     { list: 'Tools', skill: 'Jira' },
     { list: 'Tools', skill: 'Kubernetes' },
-    { list: 'Tools', skill: 'Mocha' },
-    { list: 'Tools', skill: 'MongoDB' },
-    { list: 'Tools', skill: 'Supertest' },
+    { list: 'Frameworks', skill: 'Supertest' },
     { list: 'Tools', skill: 'Swagger/OpenAPI 3' },
   ];
 
-  public get skillList() {
-    return this._skillList;
+  public get skillList(): SkillModel[] {
+    if (environment.production) {
+      return this._skillList;
+    } else return this._skillListDefault;
   }
   public set skillList(value) {
-    this._skillList = value;
+    if (environment.production) {
+      this._skillList = value;
+    } else this._skillListDefault = value;
   }
 }
