541a32f8160547de2933a85575760069521f8629
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2024-05-04 02:26:37 -0400

    update db function and designs
    Functions:
     - remove settings that don't work
     - add health and uptime checks
     - split routes that should have authentication from others
     - update  middleware
     - add database response model
    
    UI
    - update credentials page to be more dynamic
    - change font
    - update dark theme

---
+++ functions/src/models/post-response.model.ts
@@ -0,0 +1,21 @@
+import { QueryResult } from 'pg';
+
+export class PostDBResponse {
+  posts: QueryResult<any> | undefined = {
+    rows: [],
+    command: '',
+    rowCount: null,
+    oid: 0,
+    fields: [],
+  };
+
+  createBlankResponse(): QueryResult<any> | undefined {
+    return {
+      rows: [],
+      command: '',
+      rowCount: null,
+      oid: 0,
+      fields: [],
+    };
+  }
+}
