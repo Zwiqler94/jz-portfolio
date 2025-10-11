a80183b86ac93a1828542ffde56fe650c251f264
Zwiqler94 <10226084+Zwiqler94@users.noreply.github.com>
2025-10-11 00:05:01 -0400

    chore: merge dev

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,12 @@
 module.exports = function (config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    frameworks: ["jasmine"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
-      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       jasmine: {
@@ -18,10 +17,7 @@ module.exports = function (config) {
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
-        random: false,
-        stopOnSpecFailure: true,
       },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
@@ -29,10 +25,15 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/jz-portfolio"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text" }],
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
-    reporters: ["progress", "coverage", "kjhtml"],
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
     browsers: ["Chrome"],
+    singleRun: false,
     restartOnFileChange: true,
   });
 };
